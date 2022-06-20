8032617eb686e9ea963f2b63de9c2d6f27a5226e
mashabak <malesaody@gmail.com>
2022-06-20 04:25:10 +0200

    go and update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function onclickmenu(){
+  document.getElementById("men").classList.toggle("change")
+
+document.getElementById("nav").classList.toggle("change");
+
+document.getElementById("menu-bg").classList.toggle("change-bg");
+
+}
 
