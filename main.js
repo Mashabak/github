ab074f21eaf78219b66a615272b7f8fd10090baa
mashabak <malesaody@gmail.com>
2022-07-03 08:21:44 +0200

    hus

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-function onclickmenu(){
-  document.getElementById("men").classList.toggle("change")
-
-document.getElementById("nav").classList.toggle("change");
-
-document.getElementById("menu-bg").classList.toggle("change-bg");
-
+window.addEventListener("scroll").function () { var all2 = document.querySelector("all2"); all2.classList.toggle("sticky", window.scroll > 0); 
 }
+
 
